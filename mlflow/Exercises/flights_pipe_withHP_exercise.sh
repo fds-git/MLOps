07837814d91e6e29a182e46ddb61f02d47d29675
Dima <fds0303@mail.ru>
2022-12-07 07:50:38 +0300

    opd

---
+++ mlflow/Exercises/flights_pipe_withHP_exercise.sh
@@ -0,0 +1,11 @@
+# if you plan to run this script file
+# don't forget to provide neccessary execution rights, i.e.
+# chmode +x ./flights_pipe_withHP_exercise.sh
+
+#Change names in <> for the correct values
+
+spark-submit \
+--jars mlflow-spark-1.27.0.jar \
+<Your main app file>.py \
+--train_artifact "s3a://mlflow-test/data/flights-larger.csv" \
+--output_artifact <Your folder name with the pipeline>
